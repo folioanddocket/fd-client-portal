33e36d0df71a348bd51d0bd3f6639c6eaba04346
folioanddocket <folioanddocket@gmail.com>
2025-09-18 13:19:53 -0700

    feat: add sign-up page

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,4 @@
+import { SignUp } from "@clerk/nextjs";
+export default function Page() {
+  return <div style={{display:"flex",justifyContent:"center"}}><SignUp routing="path" path="/sign-up" /></div>;
+}
