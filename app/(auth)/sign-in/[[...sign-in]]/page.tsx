3593257e8067ef6dfd3983f66cff29bfc7405469
folioanddocket <folioanddocket@gmail.com>
2025-09-18 13:15:40 -0700

    feat: add sign-in page

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,4 @@
+import { SignIn } from "@clerk/nextjs";
+export default function Page() {
+  return <div style={{display:"flex",justifyContent:"center"}}><SignIn routing="path" path="/sign-in" /></div>;
+}
